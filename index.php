2196396be8267a0bca5ff8824b0876b9913897d7
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-11-07 13:03:22 +0800

    Updates

--- index.php
+++ index.php
@@ -98,7 +98,7 @@
                 <h4>The Most Popular Sales Marathon Tool</h4>
                 <p style="font-weight: 300; font-family: "Open Sans";">Millions of people use SalesMarathon to clarify thinking, manage complex information, run brainstorming and get work organized.</p>
 
-                <img src="<?=asset( 'img/sales-marathon.jpg' )?>" alt="Sales Marathon, Manager, Sales Representative"/>
+                <img src="<?=asset( 'img/sales-marathon.jpg' )?>" style="widows:80%;" alt="Sales Marathon, Manager, Sales Representative"/>
             </div>
         </div>
 
